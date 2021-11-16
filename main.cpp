3b79e6b0b74a4076ac097448777e0b19ad4ecc0a
Anders Cedronius <anders.cedronius@tickup.se>
2021-11-16 15:15:11 +0100

    add http(s) and add json parser

---
+++ main.cpp
@@ -0,0 +1,39 @@
+
+//  curl -d '{"key1":10, "key2":20}' -H "Content-Type: application/json" -X POST http://127.0.0.1:8080/interface
+
+#include <iostream>
+#include "json.h"
+#include "httplib.h"
+#include <string>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    httplib::Server svr;
+
+    svr.Post("/interface", [](const httplib::Request &rRequest, httplib::Response &rResponce) {
+
+        std::cout << rRequest.body << std::endl;
+
+        auto lParsedJson = nlohmann::json::parse(rRequest.body);
+
+        nlohmann::json lResponce;
+
+        for (auto& [key, value] : lParsedJson.items()) {
+            std::cout << key << " : " << value << "\n";
+            if (key.compare("key1")) {
+                lResponce["key1"] = (uint64_t)value + 1;
+            }
+            if (key.compare("key2")) {
+                lResponce["key2"] = (uint64_t)value + 1;
+            }
+
+        }
+
+        rResponce.set_content(lResponce.dump(), "application/json");
+    });
+
+    svr.listen("0.0.0.0", 8080);
+
+    return 0;
+}
